1bbbdc49e88c513cef9b4dab220042e170f8834d
RazbeR <rusber03102003@gmail.com>
2020-04-27 14:57:37 +0300

    Изменено оформление страницы. Добавлены формы регистрации и авторизации.

--- Main.py
+++ Main.py
@@ -14,5 +14,15 @@ def load_main_page():
     return render_template('main_page.html')
 
 
+@app.route('/registration')
+def load_registration_form():
+    return render_template('registration_form.html', user_authorised=True)
+
+
+@app.route('/login')
+def login_to_account():
+    return render_template('login_form.html', user_authorised=True)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
